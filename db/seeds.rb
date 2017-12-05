9d64ffca2d9d9f33a15883c0c10a57274880ec7e
Deetss <dylan817@live.com>
2017-12-05 22:38:53 +0000

    Sets up flights/airports and associations, also sets up index form

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Airport.create([{ code: 'DFW', name: "Dallas/Ft. Worth"} , 
+                           { code: 'YUL', name: "Montréal"} ,
+                           { code: 'ATL', name: "Atlanta"} ,
+                           { code: 'DEN', name: "Denver"} ,
+                           { code: 'LAX', name: "Los Angeles"} ,
+                           { code: 'AAH	', name: "Aachen"} ,
+                           { code: 'PEK', name: "Beijing"} ,
+                           { code: 'LHR', name: "London"}
+                           ])
+                           
+Flight.create([ { from_airport_id: 2, to_airport_id: 3, flight_duration: 1, start_time: 1.week.ago },
+                { from_airport_id: 3, to_airport_id: 1, flight_duration: 1, start_time: 2.days.ago },
+                { from_airport_id: 4, to_airport_id: 6, flight_duration: 1, start_time: 3.weeks.ago },
+                { from_airport_id: 8, to_airport_id: 1, flight_duration: 1, start_time: 1.day.from_now },
+                { from_airport_id: 7, to_airport_id: 5, flight_duration: 1, start_time: 1.week.from_now }])
